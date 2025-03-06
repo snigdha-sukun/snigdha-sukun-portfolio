2ac7b762a3da6c02e1fb9f7870971250f11bd64a
snigdha-sukun <ssukun@infinitaslearning.com>
2025-03-06 18:46:44 +0100

    responsive 2

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-// === Mobile Nav Toggle (for hamburger menu) ===
-const navToggle = document.getElementById("nav-toggle");
-const navMenu = document.getElementById("nav-menu");
+const menuHamburger = document.getElementById('menu_hamburger');
+const closeMenu = document.getElementById('close_menu');
+const nav_menu = document.getElementById('nav_menu');
 
-navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("open");
-    navToggle.classList.toggle("open");
+menuHamburger.addEventListener("click", () => {
+    nav_menu.style.display = "block";
+});
+
+closeMenu.addEventListener("click", () => {
+    nav_menu.style.display = "none";
 });
